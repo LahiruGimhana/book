e9bb338cde4c65006806847f6a464f89c4c1fff7
lahirugimhana95@gmail.com <lahirugimhana95@gmail.com>
2020-12-20 18:26:50 +0530

    commit_two

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
     super(props);
     this.state={
       items: [],
-      name:"sahan",
       isLoaded:false,
     }
   }
@@ -16,41 +15,47 @@ class App extends Component {
 
     fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=EHWvdxhhBkAzsMfcGFvYXxdwaOMG8JZH')
     .then(response => response.json())
-    .then(data => console.log(data.results.books))
-    .then(json=>{
-        this.setState({
-          items:json,
-          isLoaded: true,
+      .then(data => {
+        console.log(data.results.books)
+             this.setState({
+            items:data.results.books,
+            isLoaded: true,
         })
-    });
-      // .then(json=>{
-      //     console.log(json);
-      //     this.setState({
-      //      // isLoaded: true,
-      //      // items: json,
-      //    // })
-      // });
+      })
+   ; 
   }
   render(){
 
-    var { isLoaded, items }=this.state;
+    var {  isLoaded, items }=this.state;
     
 
     if(!isLoaded){
-      return <div>Loading..it</div>;
+      return <div>
+          Loading..it
+        </div>;
     }
     
      else{
       return(
         <div className="App">
-            <ul>
-                
-               data loaded
-               {/* {{thi}} */}
+          <h1 class="topic">The New York Times best-selling books list</h1>
+            <ul class="list">
+            { items.map((item,index ) =>
+                <div class="row">
+                   <img style={{width: 175, height: 175}}  src={item.book_image}  />
+                  <div class="col-sm"><li key={index}>{ item.rank }</li><hr/></div>
+                  <div class="col-sm"><li key={index}>{ item.title }</li></div>
+                  <div class="col-sm"><li key={index}>{ item.author }</li></div>
+                  <div class="col-sm"><br/></div>
+      
+                  </div>
+               )
+              }
             </ul>
         </div>
       );
     }
-  } }
+  } 
+}
 
 export default App

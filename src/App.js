c98f294472898f9f86a1b6bdaabbeb7cbabccc40
lahirugimhana95@gmail.com <lahirugimhana95@gmail.com>
2020-12-20 14:55:24 +0530

    https://github.com/LahiruGimhana/best-selling-books/upload/main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
-export default App;
+  constructor(props){
+    super(props);
+    this.state={
+      items: [],
+      name:"sahan",
+      isLoaded:false,
+    }
+  }
+
+  componentDidMount(){
+
+    fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=EHWvdxhhBkAzsMfcGFvYXxdwaOMG8JZH')
+    .then(response => response.json())
+    .then(data => console.log(data.results.books))
+    .then(json=>{
+        this.setState({
+          items:json,
+          isLoaded: true,
+        })
+    });
+      // .then(json=>{
+      //     console.log(json);
+      //     this.setState({
+      //      // isLoaded: true,
+      //      // items: json,
+      //    // })
+      // });
+  }
+  render(){
+
+    var { isLoaded, items }=this.state;
+    
+
+    if(!isLoaded){
+      return <div>Loading..it</div>;
+    }
+    
+     else{
+      return(
+        <div className="App">
+            <ul>
+                
+               data loaded
+               {/* {{thi}} */}
+            </ul>
+        </div>
+      );
+    }
+  } }
+
+export default App
